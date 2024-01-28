ac1f1c05929559c4d43d62dbc22110f324e507ad
dev-encourt <bethencourtbrian@gmail.com>
2024-01-28 08:54:31 -0500

    JS challenges 1-4 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, number) {
+    let repeatedString = ""; 
 
-};
+    if (number < 0) {
+        return "ERROR";
+    } else if (number === 0) {
+        return repeatedString;
+    } else {
+        for (let i = 0; i < number; i++) {
+            repeatedString += stringToRepeat;
+        }
+    }
+
+    return repeatedString;
+};  
 
 // Do not edit below this line
 module.exports = repeatString;
